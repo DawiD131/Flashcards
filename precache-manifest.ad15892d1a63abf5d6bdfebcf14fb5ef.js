b31163679fa9bdc4961671107fba25ccec3ecea8
Dawid Kutwin <dawidkutwin@MacBook-Air-Dawid.local>
2020-10-01 19:21:37 +0200

    Updates

---
+++ precache-manifest.ad15892d1a63abf5d6bdfebcf14fb5ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3e9ef9e698eb1c6b78ef7c1aa9fee4",
+    "url": "/Flashcards/index.html"
+  },
+  {
+    "revision": "d426127afef4a7b4ec4a",
+    "url": "/Flashcards/static/css/main.20ac4ad5.chunk.css"
+  },
+  {
+    "revision": "3ed046a0e054e66ea3d7",
+    "url": "/Flashcards/static/js/2.b7f0eadc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Flashcards/static/js/2.b7f0eadc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d426127afef4a7b4ec4a",
+    "url": "/Flashcards/static/js/main.281fd21a.chunk.js"
+  },
+  {
+    "revision": "3ee0f10236e61cb1d863",
+    "url": "/Flashcards/static/js/runtime-main.d23f0565.js"
+  }
+]);
